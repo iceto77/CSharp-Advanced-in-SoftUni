0f79fb1ce2ab881f7e5e9bef68a01378f4703acc
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-09-17 00:23:40 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/1. Reverse Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> reverceString = new Stack<char>();
+            foreach (char ch in input)
+            {
+                reverceString.Push(ch);
+            }
+            while (reverceString.Count > 0)
+            {
+                Console.Write(reverceString.Pop());
+            }
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();            
+            string[] numbers = input.Split().ToArray();            
+            Stack<int> integerStack = new Stack<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                integerStack.Push(int.Parse(numbers[i]));
+            }
+            input = Console.ReadLine().ToLower();
+            while (input != "end")
+            {
+                string[] command = input.Split().ToArray();
+                switch (command[0])
+                {
+                    case "add":
+                        integerStack.Push(int.Parse(command[1]));
+                        integerStack.Push(int.Parse(command[2]));
+                        break;
+                    case "remove":
+                        int targetNum = int.Parse(command[1]);
+                        if (targetNum <= integerStack.Count)
+                        {
+                            for (int i = 0; i < targetNum; i++)
+                            {
+                                integerStack.Pop();
+                            }
+                        }
+                        break;
+                }
+                input = Console.ReadLine().ToLower();
+            }
+            int sum = 0;
+            foreach (int item in integerStack)
+            {
+                sum += item;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
