65497ad3d330b779b4cbfde95b650940511486f0
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-09-18 03:08:13 +0300

    Add files via upload

---
+++ Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Maximum_and_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int lineNums = int.Parse(input);
+            Stack<int> elements = new Stack<int>();
+            for (int i = 0; i < lineNums; i++)
+            {
+                input = Console.ReadLine();
+                if (input =="2")
+                {
+                    if (elements.Count > 0)
+                    {
+                        elements.Pop();
+                    }
+                }
+                else if (input == "3")
+                {
+                    int maxElement = int.MinValue;
+                    foreach (var item in elements)
+                    {
+                        if (maxElement < item)
+                        {
+                            maxElement = item;
+                        }
+                    }
+                    Console.WriteLine(maxElement);
+                }
+                else if (input == "4")
+                {
+                    int minElement = int.MaxValue;
+                    foreach (var item in elements)
+                    {
+                        if (minElement > item)
+                        {
+                            minElement = item;
+                        }
+                    }
+                    Console.WriteLine(minElement);
+                }
+                else
+                {
+                    int[] command = input.Split().Select(int.Parse).ToArray();
+                    if (command[0] == 1)
+                    {
+                        elements.Push(command[1]);
+                    }
+                }
+
+            }
+            Console.WriteLine(string.Join(", ", elements));
+        }
+    }
+}
