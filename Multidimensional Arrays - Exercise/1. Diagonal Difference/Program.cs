0c542b43a10f94953a7d3c250ee3a8697b92173e
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-09-23 01:23:51 +0300

    Add files via upload

---
+++ Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int rowscols = int.Parse(input);
+            int[,] myMatrix = new int[rowscols, rowscols];
+            for (int i = 0; i < myMatrix.GetLength(0); i++)
+            {
+                input = Console.ReadLine();
+                int[] currentRow = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int j = 0; j < myMatrix.GetLength(0); j++)
+                {
+                    myMatrix[i, j] = currentRow[j];
+                }
+            }
+            int primaryDiagonal = 0;
+            int secondaryDiagonal = 0;
+            for (int i = 0; i < myMatrix.GetLength(0); i++)
+            {
+                primaryDiagonal += myMatrix[i, i];
+                secondaryDiagonal += myMatrix[i, myMatrix.GetLength(1) - 1 - i];
+            }
+            int diagonalDifference = Math.Abs(primaryDiagonal - secondaryDiagonal);
+            Console.WriteLine(diagonalDifference);
+        }
+    }
+}
---
+++ Multidimensional Arrays - Exercise/2. Squares in Matrix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[] rowscols = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = rowscols[0];
+            int cols = rowscols[1];
+            char[,] myMatrix = new char[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                input = Console.ReadLine();
+                char[] curentLine = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int j = 0; j < cols; j++)
+                {
+                    myMatrix[i, j] = curentLine[j];
+                }
+            }
+            int squaresOfEqualChars = 0;
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    if (myMatrix[i,j] == myMatrix[i + 1, j] && myMatrix[i, j] == myMatrix[i, j + 1] && myMatrix[i, j] == myMatrix[i + 1, j + 1])
+                    {
+                        squaresOfEqualChars++;
+                    }
+                }
+            }
+            Console.WriteLine(squaresOfEqualChars);
+        }
+    }
+}
