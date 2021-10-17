7b35613410557dff5e7d90dec7fcbed1ddd269d9
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-10-17 23:45:01 +0300

    Add files via upload

---
+++ Advanced - Exam Preparation/03.StreetRacing/Race.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            Name = name;
+            Type = type;
+            Laps = laps;
+            Capacity = capacity;
+            MaxHorsePower = maxHorsePower;
+            Participants = new List<Car>();
+        }
+
+        public List<Car> Participants { get; set; }
+        public int Count => Participants.Count;
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Laps { get; set; }
+        public int Capacity { get; set; }
+        public int MaxHorsePower { get; set; }
+
+        public void Add(Car car)
+        {
+            if (FindParticipant(car.LicensePlate) == null && Capacity >= Participants.Count + 1 && MaxHorsePower >= car.HorsePower)
+            {
+                Participants.Add(car);
+            }
+        }
+        public bool Remove(string licensePlate)
+        {
+            Car car = Participants.FirstOrDefault(x => x.LicensePlate == licensePlate);
+            if (car == null)
+            {
+                return false;
+            }
+            else
+            {
+                Participants.Remove(car);
+                return true;
+            }
+        }
+        public Car FindParticipant(string licensePlate)
+        {
+            return Participants.FirstOrDefault(x => x.LicensePlate == licensePlate);
+        }
+        public Car GetMostPowerfulCar()
+        {
+            return Participants.OrderByDescending(x => x.HorsePower).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Race: {Name} - Type: {Type} (Laps: {Laps})");
+            foreach (var item in Participants)
+            {
+                sb.AppendLine(item.ToString().TrimEnd());
+            }
+            return sb.ToString();
+        }
+    }
+}
