c8165fcb506edf4d275f592232c836372f56c2e7
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-10-17 17:16:42 +0300

    Add files via upload

---
+++ Advanced - Exam Preparation/01.BirthdayCelebration/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01.BirthdayCelebration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> guestsCapacity = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Stack<int> platesOfFood = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int wastedFood = 0;
+            while (guestsCapacity.Count > 0 && platesOfFood.Count > 0)
+            {
+                int currentGuest = guestsCapacity.Peek();
+                while (currentGuest - platesOfFood.Peek() > 0)
+                {
+                    currentGuest -= platesOfFood.Pop();
+                }
+                wastedFood += platesOfFood.Pop() - currentGuest;
+                guestsCapacity.Dequeue();
+
+            }
+            if (guestsCapacity.Count == 0)
+            {
+                Console.WriteLine($"Plates: {PrintStack(platesOfFood)}");
+            }
+            else if (platesOfFood.Count == 0)
+            {
+                Console.WriteLine($"Guests: {PrintQueue(guestsCapacity)}");
+            }
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
+        }
+
+        private static string PrintQueue(Queue<int> guestsCapacity)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (guestsCapacity.Count > 0)
+            {
+                sb.Append(guestsCapacity.Dequeue() + " ");
+            }
+
+            return sb.ToString();
+        }
+
+        private static string PrintStack(Stack<int> platesOfFood)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (platesOfFood.Count > 0)
+            {
+                sb.Append(platesOfFood.Pop() + " ");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Advanced - Exam Preparation/02.TheBattleOfTheFiveArmies/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.TheBattleOfTheFiveArmies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int armorOfArmy = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+            int armyRow = 0;
+            int armyCol = 0;
+            char[][] middleWorld = new char[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                middleWorld[i] = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < middleWorld[i].Length; j++)
+                {
+                    if (middleWorld[i][j] == 'A')
+                    {
+                        armyRow = i;
+                        armyCol = j;
+                    }
+                }
+            }
+            while (armorOfArmy > 0)
+            {
+                string[] command = Console.ReadLine().Split();
+                int newOrcRow = int.Parse(command[1]);
+                int newOrcCol = int.Parse(command[2]);
+                middleWorld[newOrcRow][newOrcCol] = 'O';
+                middleWorld[armyRow][armyCol] = '-';
+                switch (command[0])
+                {
+                    case "up":
+                        if (armyRow - 1 >= 0)
+                        {
+                            armyRow--;
+                        }
+                        break;
+                    case "down":
+                        if (armyRow + 1 < middleWorld.GetLength(0))
+                        {
+                            armyRow++;
+                        }
+                        break;
+                    case "left":
+                        if (armyCol - 1 >= 0)
+                        {
+                            armyCol--;
+                        }
+                        break;
+                    case "right":
+                        if (armyCol + 1 < middleWorld[armyRow].Length)
+                        {
+                            armyCol++;
+                        }
+                        break;
+                }
+                armorOfArmy--;
+                if (middleWorld[armyRow][armyCol] == 'O')
+                {
+                    armorOfArmy -= 2;
+
+                }
+                if (middleWorld[armyRow][armyCol] == 'M')
+                {
+                    Console.WriteLine($"The army managed to free the Middle World! Armor left: {armorOfArmy}");
+                    middleWorld[armyRow][armyCol] = '-';
+                    break;
+                }
+                if (armorOfArmy <= 0)
+                {
+                    middleWorld[armyRow][armyCol] = 'X';
+                    Console.WriteLine($"The army was defeated at {armyRow};{armyCol}.");
+                    break;
+                }
+                middleWorld[armyRow][armyCol] = 'A';
+            }
+            foreach (var item in middleWorld)
+            {
+                Console.WriteLine(string.Join("", item));
+            }
+        }
+    }
+}
